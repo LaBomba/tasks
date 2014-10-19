d24e3246879b53e98a348e91af03bdc0d823f0ed
mujx <siderisk@auth.gr>
2014-10-19 18:34:55 +0300

    Fix style

--- catkin-workspace/src/simple_communication/src/manipulator.cpp
+++ catkin-workspace/src/simple_communication/src/manipulator.cpp
@@ -1,34 +1,36 @@
+#include <stdint.h>
+
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include "std_msgs/Int16.h"
 
-#include <stdint.h>
 
 /**
  * Manipulator Node for Task 1
  */
 
-void random_number_callback(const std_msgs::Int16 message)
+void modifyCallback(const std_msgs::Int16 message)
 {
-    int16_t original = message.data;
-    int32_t modified = original*original;
-    ROS_INFO("Received: %d, Squared: %d", original, modified);
+  int16_t original = message.data;
+  int32_t modified = original*original;
+  ROS_INFO("Received: %d, Squared: %d", original, modified);
 }
 
 
 int main(int argc, char **argv)
 {
-    const char *node_name = "manipulator";
-    const char *topic_name = "/test1/numbers";
-    uint32_t queue_size = 200;
+  const char* node_name = "manipulator";
+  const char* topic_name = "/test1/numbers";
+  uint32_t queue_size = 200;
 
-    ros::init(argc, argv, node_name);
+  ros::init(argc, argv, node_name);
 
-    ros::NodeHandle node_handler;
+  ros::NodeHandle node_handler;
 
-    ros::Subscriber subscriber = node_handler.subscribe(topic_name, queue_size, random_number_callback);
+  ros::Subscriber subscriber = \
+                  node_handler.subscribe(topic_name, queue_size, modifyCallback);
 
-    ros::spin();
+  ros::spin();
 
-    return 0;
+  return 0;
 }
--- catkin-workspace/src/simple_communication/src/streamer.cpp
+++ catkin-workspace/src/simple_communication/src/streamer.cpp
@@ -1,9 +1,9 @@
+#include <stdint.h>
+#include <cstdlib>
+
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include "std_msgs/Int16.h"
-
-#include <stdint.h>
-#include <cstdlib>
 
 
 /**
@@ -12,37 +12,37 @@
 
 int main(int argc, char **argv)
 {
-    const char *node_name = "streamer";
-    const char *topic_name = "/test1/numbers";
-    uint32_t queue_size = 200;
+  const char* node_name = "streamer";
+  const char* topic_name = "/test1/numbers";
+  uint32_t queue_size = 200;
 
-    // Advertise one message every second
-    float publish_rate = 1;
+  // Advertise one message every second
+  float publish_rate = 1;
 
-    ros::init(argc, argv, node_name);
+  ros::init(argc, argv, node_name);
 
-    ros::NodeHandle node_handler;
+  ros::NodeHandle node_handler;
 
-    ros::Publisher node_publisher = node_handler.advertise<std_msgs::Int16>(topic_name, queue_size);
+  ros::Publisher node_publisher = \
+                 node_handler.advertise<std_msgs::Int16>(topic_name, queue_size);
 
-    ros::Rate loop_rate(publish_rate);
+  ros::Rate loop_rate(publish_rate);
 
-    while (ros::ok())
-    {
-        std_msgs::Int16 message;
+  while (ros::ok())
+  {
+    std_msgs::Int16 message;
 
-        message.data = rand();
+    message.data = rand();
 
-        ROS_INFO("%s said: %d", node_name, message.data);
+    ROS_INFO("%s said: %d", node_name, message.data);
 
-        node_publisher.publish(message);
+    node_publisher.publish(message);
 
-        ros::spinOnce();
+    ros::spinOnce();
 
-        loop_rate.sleep();
+    loop_rate.sleep();
 
-    }
+   }
 
-    return 0;
-
+   return 0;
 }
